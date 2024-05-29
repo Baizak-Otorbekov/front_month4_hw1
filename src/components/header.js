1f5d5a127f8403375797a95a71cc71d9fa95995d
Baizak-Otorbekov <162703040+Baizak-Otorbekov@users.noreply.github.com>
2024-05-29 14:36:30 +0600

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header">
+        <nav>
+          <div>Logo</div>
+          <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+          <button>Hello</button>
+        </nav>        
+      </header>
+    )
+}
+export default Header;
---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./counter";
+
+function Main() {
+  return (
+    <section className='main'>
+      Baizak Otorbekov
+      Baizak Otorbekov
+      Baizak Otorbekov
+      Baizak Otorbekov
+      <Counter/>
+    </section>
+  )
+}
+export default Main;
